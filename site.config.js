1181278c961e609398e7ee4dfae376f5becd3fa8
Nereid <34327164+nereidc@users.noreply.github.com>
2021-07-18 12:27:24 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '190f68ffeecf49159f0fbc125e8e75c0',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Zhou Yi-Xin',
+  domain: 'https://crocus-handball-8b9.notion.site/Zhou-Yi-Xin-190f68ffeecf49159f0fbc125e8e75c0',
+  author: 'Zhou Yi-Xin',
 
   // open graph metadata (optional)
   description: 'Example site description',
